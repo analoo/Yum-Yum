001eb2c51822df970c0f7363cf6a97970c412ae1
Ana Medrano <amedrano.fernandez@gmail.com>
2020-05-13 21:05:06 -0700

    created signup and login changes with form event listeners

--- server/yumyum/src/pages/Login.js
+++ server/yumyum/src/pages/Login.js
@@ -1,5 +1,7 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import MainBody from "../components/mainBody";
+import FormMain from "../components/formMain";
+
 
 
 class Login extends Component {
@@ -9,38 +11,62 @@ class Login extends Component {
     password: ""
   };
 
-  handleInputChange = evenet => {
+  handleInputChange = event => {
+    let value = event.target.value;
+    let name = event.target.name;
 
-  }
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(`Thank you for signing up with the username: ${this.state.email}`)
+    console.log(`Thank you for signing up with the password: ${this.state.password}`)
+    this.setState({
+      email: "",
+      password: ""
+    });
+
+  };
 
 
   render() {
     return (
       <div>
+
         <MainBody />
-        <h1>Login</h1>
-        <form className="form">
-          <div className="form-group row">
-            <label for="staticEmail" className="col-sm-2 col-form-label">Email</label>
-            <div className="col-sm-10">
-              <input type="text" readonly clasName="form-control-plaintext"
-                value={this.state.firstName}
-                name="email"
-                onChange={this.handleInputChange}
-                placeholder="username" />
+        <FormMain />
+        <div className="form-div col-md-6 col-sm-12">
+          <h2>Login</h2>
+          <form className="form">
+            <div className="form-group row">
+              <label className="col-sm-2 col-form-label">Email</label>
+              <div className="col-sm-10">
+                <input type="text" id="staticEmail" className="form-control"
+                  value={this.state.email}
+                  name="email"
+                  onChange={this.handleInputChange}
+                  placeholder="email" />
+              </div>
             </div>
-          </div>
-          {/* <div className="form-group row">
-            <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-    </div>
+            <div className="form-group row">
+              <label className="col-sm-2 col-form-label">Password</label>
+              <div className="col-sm-10">
+                <input type="password" className="form-control" id="inputPassword"
+                  value={this.state.password}
+                  name="password"
+                  onChange={this.handleInputChange}
+                  placeholder="password" />
+              </div>
             </div>
-       */}
-      </form>
-          </div>
-        )
-      };
-    }
-    
-    export default Login;
+            <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Login</button>
+          </form>
+        </div>
+      </div>
+    )
+  };
+}
+
+export default Login;
