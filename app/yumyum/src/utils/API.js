86f55641858c075c5ad2b49dd225493ae5c5f2ef
ChinDowns <webdev@chindowns.com>
2020-05-15 06:31:01 -0700

    Move API.js from SERVERto APP inside of yumyum/util

---
+++ app/yumyum/src/utils/API.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export default {
+    getUserRecipes: function (id) {
+        return axios.get(`/api/${id}/recipes`);
+    },
+
+    getAllRecipes: function () {
+        return axios.get(`/api/recipes`);
+    },
+
+    postRecipe: function (recipe) {
+        return axios.post("/api/recipes/new", recipe)
+
+    }
+
+};
