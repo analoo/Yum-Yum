ebd7b08e14d0f115c53c61f353fcca96c804c26c
ChinDowns <webdev@chindowns.com>
2020-05-15 08:11:33 -0700

    Write to Recipes Successful

---
+++ app/seeders/seed.js
@@ -0,0 +1,27 @@
+// Run this through POSTMAN 
+
+// Users
+{
+    name: "Fred Flintstone",
+    email: "fred@email.com",
+    username: "fred"
+}
+
+
+// Recipes
+{
+name: "Brooklyn BBQ"
+photo: "public/assets/images-recipe/brooklynbbq.jpg",
+servingSize: 16,
+activeTime: 20,
+totalTime: 25,
+directions: "Put all the ingredients for the barbecue sauce in small pot and cook over medium-low heat until the sauce thickens and the flavors combine, about for 20 minutes.
+    Whisk together the vinegar, honey and oil, in a medium bowl.Stir in the celery salt and black pepper, to taste.Add the slaw mix and onions and toss together.Let the salad wilt up to 10 to 15 minutes.
+    Preheat a grill or griddle to medium.
+    Put the meat in a bowl with the grated onion and season with steak seasoning or salt and pepper, to taste.In a small bowl combine the Worcestershire with the herbs, chili powder and beer.Pour the herb mixture over the meat and mix thoroughly.Form the meat mixture into 12 sliders and drizzle with vegetable oil or olive oil.Put the burgers on the grill or griddle and cook for a few minutes on each side.Baste the burgers liberally with barbecue sauce.Top the burgers with Cheddar and let it melt, if you prefer cheese burgers.
+    Serve the burgers on slider rolls topped with pickle slices and the slaw.
+",
+ingredients: [
+    
+]
+}
