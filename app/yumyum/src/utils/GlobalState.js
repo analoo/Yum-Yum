011ddedaad995a0082b53a3151f94bc7c7d84793
Ana Medrano <amedrano.fernandez@gmail.com>
2020-05-19 15:55:30 -0700

    added user state  and defined add recipe, favorite loading

--- app/yumyum/src/utils/GlobalState.js
+++ app/yumyum/src/utils/GlobalState.js
@@ -1,13 +1,14 @@
 import React, {createContext, useReducer, useContext} from "react";
 
 import {
+    ADD_RECIPE,
+    ADD_FAVORITE,
+    LOADING,
     UPDATE_RECIPE,
     REMOVE_RECIPE,
     SET_CURRENT_RECIPE,
     COPY_RECIPE,
-    ADD_RECIPE,
     LOADING,
-    ADD_FAVORITE,
     REMOVE_FAVORITE
 } from ".actions";
 
@@ -16,8 +17,23 @@ const {Provider} = SessionContext;
 
 const reducer = (state,action) => {
     switch (action.type) {
-        
-        
+        case ADD_RECIPE:
+            return {
+                ...state,
+                currentRecipe: action.recipe,
+                loading: false
+            };
+        case ADD_FAVORITE:
+            return {
+                ...state,
+                favorites: [action.recipe, ...state.favorites],
+                loading: false
+            }
+        case LOADING:
+            return {
+                ...state,
+                loading: true
+            }
         default:
             return state
 
@@ -48,11 +64,29 @@ const SessionProvider = ({value = [], ...props}) => {
             originalRecipeID: "",
             rating: "",
             ingredients: [],
-            tags: []
-
+            tags: [],
+        },
+        currentIngredients: {
+            id: "",
+            name: "",
+            amount: "",
+            measurement: ""
+        },
+        currentTags:{
 
         },
         userGenerated: [],
         userFavorites: [],
-    })
+        currentSearch: "",
+        // AMF: I still do not know wht loading does but I am adding it here for consistency
+        loading: false
+    });
+
+    return <Provider value={[state, dispatch]} {...props} />
 }
+
+const useSessionContext = () => {
+    return useContext(SessionContext);
+}
+
+export { SessionProvider, useSessionContext};
