fe014a2242603b59fd4fa0ae4a4b12ea6501b9ba
Ana Medrano <amedrano.fernandez@gmail.com>
2020-05-19 13:14:15 -0700

    created step and ingredients components

---
+++ app/yumyum/src/components/Form/Add-Ingredient.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+function AddIngredient(props) {
+    return (
+        <div className="row">
+            {/* <input type="text" className="form-control col-md-3" id="exampleFormControlInput1"
+                value={getRecipe.ingredients}
+                name="ingredients"
+                onChange={e => setRecipe({ ...getRecipe, ingredients: e.target.value })}
+                placeholder="Ingredients Name" />
+            <input type="text" className="form-control col-md-3" id="exampleFormControlInput1"
+                value={getRecipe.ingredients}
+                name="ingredients"
+                onChange={e => setRecipe({ ...getRecipe, ingredients: e.target.value })}
+                placeholder="Quantity" />
+            <input type="text" className="form-control col-md-3" id="exampleFormControlInput1"
+                value={getRecipe.ingredients}
+                name="ingredients"
+                onChange={e => setRecipe({ ...getRecipe, ingredients: e.target.value })}
+                placeholder="Measurement" /> */}
+            <button>+</button>
+        </div>
+    )
+}
+
+export default AddIngredient;
