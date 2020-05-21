2ba3f5210513bcc11373859fa349547f753a056d
Ana Medrano <amedrano.fernandez@gmail.com>
2020-05-21 11:58:43 -0700

    moved details around a bit

--- app/yumyum/src/components/Recipe/index.js
+++ app/yumyum/src/components/Recipe/index.js
@@ -1,29 +1,27 @@
 import React from "react";
 import "./recipe.css"
 import { OLElement, LIElement, ULElement } from "./listElem" 
-import { useSessionContext } from "../../utils/GlobalState";
 
 
-function RecipeMain() {
-    const [state,dispatch] = useSessionContext();
+function RecipeMain(props) { 
 
-    let ingredients = state.currentIngredients
-    let steps = state.currentRecipe.directions.split("/")
-    let tags = state.currentTags;
+    let ingredients = props.currentIngredients
+    let steps = props.currentRecipe.directions.split("/")
+    let tags = props.currentTags;
 
     return (
         <div className="container recipe-container">
-            <h3>{state.currentRecipe.name}</h3>
+            <h3>{props.currentRecipe.name}</h3>
             <div className="row">
                 <div className="col-md-4">
-                    <img className="detail-img" src={state.currentRecipe.photo} />
+                    <img className="detail-img" src={props.currentRecipe.photo} />
                     {tags.map(el => (
                         <p className="tag" key={el.tag}>{el.tag}</p>
                     ))}
                 </div>
                 <div className="col-md-8">
                     <p className="rec-label">Description</p>
-                    <p className="description">{state.currentRecipe.description}</p>
+                    <p className="description">{props.currentRecipe.description}</p>
                     <p className="rec-label">Ingredients</p>
                     <ULElement>
                         {ingredients.map(ingredient => (
--- app/yumyum/src/pages/Details.js
+++ app/yumyum/src/pages/Details.js
@@ -1,20 +1,36 @@
 import React, {useEffect, useState} from "react";
 import MainBody from "../components/Containers/mainBody"
 import RecipeMain from "../components/Recipe/index"
+import API from "../utils/API"
 import { useSessionContext } from "../utils/GlobalState";
 
 
 
 const Details = (props) => {
-    const [state,dispatch] = useSessionContext();
+    const [currentRecipe, setCurrentRecipe] = useState({})
 
+    console.log(props.match.params.id)
 
-    console.log(state.currentRecipe)
+    useEffect( () => {
+        loadRecipes();
+      }, [])
+
+    function loadRecipes(){
+        API.getSingleRecipe(props.params.match.id)
+        .then(result => {
+            setCurrentRecipe(result.data)
+            console.log(result.data)})
+        .catch(err => console.log(err))
+    }
+    
+
+    
+
 
     return (
         <div>
             <MainBody>
-                {!state.currentRecipe.id
+                {currentRecipe === 0
                 ? <h3>No Results</h3>
                 :
                 <RecipeMain />
